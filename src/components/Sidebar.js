af92c46666adcad61d02cd3f4823efe5d1fc5a65
Lotfi <lotfi.doucanef@pertimm.com>
2021-06-16 11:51:19 +0200

    [ADD] Sidebar and sidebarOptions had been created

---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/*Twitter icon */}
+            <TwitterIcon />
+
+            <SidebarOption />
+            <SidebarOption />
+            <SidebarOption />
+            <SidebarOption />
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+
+            {/*Button */}
+        </div>
+    );
+}
+
+export default Sidebar;
