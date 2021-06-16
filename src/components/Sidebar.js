536fa392e782e9d59ba0e220a8903dbbcf49a8b6
Lotfi <lotfi.doucanef@pertimm.com>
2021-06-16 16:41:18 +0200

    [ADDED] middlepage has been added

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import {Button} from "@material-ui/core";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/*Twitter icon */}
+            <TwitterIcon />
+
+            <SidebarOption active text="Home" />
+            <SidebarOption text="Explore" />
+            <SidebarOption text="Notifications" />
+            <SidebarOption text="Messages" />
+            <SidebarOption text="Bookmarks" />
+            <SidebarOption text="Lists" />
+            <SidebarOption text="Profile" />
+            <SidebarOption text="More" />
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+
+            {/*Button */}
+            <Button variant="outlined" className="tweet_sidebar" fullWidth>Tweet</Button>
+        </div>
+    );
+}
+
+export default Sidebar;
