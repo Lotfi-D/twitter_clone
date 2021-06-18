69a6b0f16cad817841c859b01cb87a72b60103d7
Lotfi <lotfi.doucanef@pertimm.com>
2021-06-18 12:08:20 +0200

    [UPDATE] avatar has been changed

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -26,7 +26,7 @@ function TweetBox() {
         <div className="tweetBox">
             <form>
                 <div className="tweetBox_input">
-                <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+                <Avatar src="https://pbs.twimg.com/profile_images/684869598194302976/mPoV1q0H_400x400.jpg" />
                 <input 
                 onChange={(e)=> setTweetMessage(e.target.value)}
                 value={tweetMessage} 
